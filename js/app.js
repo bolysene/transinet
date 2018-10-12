738a6ddc3491f339db9ffa8da602417b05cfada8
Boly <seneboly@gmail.com>
2018-10-12 20:59:45 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function() {
+  enable_cb();
+  $("#group1").click(enable_cb);
+});
+
+function enable_cb() {
+  if (this.checked) {
+    $("input.group1").removeAttr("disabled");
+  } else {
+    $("input.group1").attr("disabled", true);
+  }
+}
+
